dbd3ce33edeebc8a6d704c95d1ed3d7c2f3ee922
ihriel.mosko <80472864+ihrielmosko@users.noreply.github.com>
2022-03-25 11:05:10 -0300

    codigo inicial
    vou trabalhar em cima dele

--- cenario.c
+++ cenario.c
@@ -1,8 +1,25 @@
+#include <windows.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <windows.h>
+#include <ctype.h>
 
-int main () {
-    printf("%c", 12);
-    
+
+void gotoxy (int x, int y)
+{
+
+    COORD coord;
+    coord.X = x;
+    coord.Y = y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+
+}
+int main (){
+
+gotoxy(10,10);
+printf("banana pacas");
+system ("pause"); //pausar  na tela - somente
+
+
+
+return 0;
 }
