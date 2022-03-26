c54d65bee38de7ef8eab47fe78173870216c728b
ihriel_mosko <ihriel.mosko@gmail.com>
2022-03-25 23:50:09 -0300

    agora tem textura

--- cenario.c
+++ cenario.c
@@ -14,32 +14,37 @@ void gotoxy (int x, int y)
 
 }
 int main (){
+    //gotoxy(0,0);
+    //printf("%c", 219);
 
-//gotoxy(0,0);
-//printf("%c", 219);
-///*
-int i;
-for (i = 0; i <= 10; i++) {
-    gotoxy(0,i);
-    printf("%c", 219);
-}
-for (i = 0; i <= 20; i++) {
-    gotoxy(i,0);
-    printf("%c", 219);
-}
-for (i = 0; i <= 10; i++) {
-    gotoxy(20,i);
-    printf("%c", 219);
-}
-for (i = 0; i <= 20; i++) {
-    gotoxy(i,10);
-    printf("%c", 219);
-}
-//*/
-printf("\n\n");
-system ("pause"); //pausa o sistema
+    int i;
+    int i2;
+    ///*
+    for (i = 0; i <= 10; i++) {
+        gotoxy(0,i);
+        printf("%c", 219);
+    }
+    for (i = 0; i <= 20; i++) {
+        gotoxy(i,0);
+        printf("%c", 219);
+    }
+    for (i = 0; i <= 10; i++) {
+        gotoxy(20,i);
+        printf("%c", 219);
+    }
+    for (i = 0; i <= 20; i++) {
+        gotoxy(i,10);
+        printf("%c", 219);
+    }
+    for (i = 1; i <= 19; i++) {
+        for (i2 = 1; i2 <= 9; i2++) {
+            gotoxy(i,i2);
+            printf("%c", 176);
+        }
+    }
+    //*/
 
-
-
-return 0;
+    printf("\n\n");
+    system ("pause");
+    return 0;
 }
