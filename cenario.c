031601ebd779e9588afd5a7cf667aeb26ecdad29
ihriel_mosko <ihriel.mosko@gmail.com>
2022-03-29 12:55:07 -0300

    início do teste

--- cenario.c
+++ cenario.c
@@ -1,7 +1,5 @@
 #include <windows.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
 
 
 void gotoxy (int x, int y)
@@ -14,12 +12,14 @@ void gotoxy (int x, int y)
 
 }
 int main (){
-    //gotoxy(0,0);
-    //printf("%c", 219);
 
-    int i;
-    int i2;
-    ///*
+    //int i, i2;
+    int i, i2, x, y;
+    printf("digite o valor de x\n");
+    scanf("%d", &x);
+    printf("digite o valor de y\n");
+    scanf("%d", &y);
+    /*
     for (i = 0; i <= 10; i++) {
         gotoxy(0,i);
         printf("%c", 219);
@@ -42,8 +42,29 @@ int main (){
             printf("%c", 176);
         }
     }
-    //*/
-
+    */
+    for (i = 0; i <= x; i++) {
+        gotoxy(i,0);
+        printf("%c", 219);
+    }
+    for (i = 0; i <= y; i++) {
+        gotoxy(0,i);
+        printf("%c", 219);
+    }
+    for (i= 0; i <= x; i++) {
+        gotoxy(i,y);
+        printf("%c", 219);
+    }
+    for (i= 0; i <= y; i++) {
+        gotoxy(x,i);
+        printf("%c", 219);
+    }
+    for (i = 1; i = x; i++) {
+        for (i2 = 1; i2 = y; i2++) {
+            gotoxy(i,i2);
+            printf("%c", 176);
+        }
+    }
     printf("\n\n");
     system ("pause");
     return 0;
