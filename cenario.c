a47eecb4ba14d7a478d6a2e20b7cfcdedd1062e6
ihriel_mosko <ihriel.mosko@gmail.com>
2022-04-08 12:31:21 -0300

    system cls

--- cenario.c
+++ cenario.c
@@ -14,11 +14,16 @@ void gotoxy (int x, int y)
 int main (){
 
     //int i, i2;
+    ///*
     int i, i2, x, y;
+    
+    system("cls");
+
     printf("digite o valor de x\n");
     scanf("%d", &x);
     printf("digite o valor de y\n");
     scanf("%d", &y);
+    //*/
     /*
     for (i = 0; i <= 10; i++) {
         gotoxy(0,i);
@@ -43,6 +48,10 @@ int main (){
         }
     }
     */
+   ///*
+
+   system("cls");
+   
     for (i = 0; i <= x; i++) {
         gotoxy(i,0);
         printf("%c", 219);
@@ -59,13 +68,17 @@ int main (){
         gotoxy(x,i);
         printf("%c", 219);
     }
+    //*/
+    /*
     for (i = 1; i = x; i++) {
         for (i2 = 1; i2 = y; i2++) {
             gotoxy(i,i2);
             printf("%c", 176);
         }
     }
+    */
     printf("\n\n");
     system ("pause");
     return 0;
 }
+//167 caracteres
